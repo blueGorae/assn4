a4216ae0934572964a31a8373ae1adf555bd6d9a
seongtae0205 <seongtae0205@postech.ac.kr>
2019-04-16 20:07:32 +0900

    Link to shader

--- main.cpp
+++ main.cpp
@@ -1,21 +1,150 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
 #include "GL/glew.h"
 #include "GL/freeglut.h"
-using namespace std;
+#include "glm/glm.hpp"
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include "GL/glut.h"
 
-void renderScene(void) { 
-	glClear(GL_COLOR_BUFFER_BIT); 
+
+#define BUFFER_OFFSET(offset) ((GLvoid*) (offset))
+
+using namespace std;
+using namespace glm;
+
+mat4 ctm;
+GLchar vertexShaderFile[] = "shader/my.vert.glsl";
+GLchar fragShaderFile[] = "shader/my.frag.glsl";
+GLuint myProgramObj;
+
+bool LoadShaders(const char * vertexShaderFile, const char * fragShaderFile) {
+	GLuint myVertexObj = glCreateShader(GL_VERTEX_SHADER);
+	GLuint myFragObj = glCreateShader(GL_FRAGMENT_SHADER);
+
+
+	//Read Vertex File
+	string vertexShaderCode;
+	ifstream vertexShaderStream;
+
+	vertexShaderStream.open(vertexShaderFile, ifstream::in);
+
+	if (vertexShaderStream.is_open()) {
+		stringstream sstr;
+		sstr << vertexShaderStream.rdbuf();
+		vertexShaderCode = sstr.str();
+		vertexShaderStream.close();
+	}
+	else {
+		cout << "File does not open"<<endl;
+		getchar();
+		return false;
+	}
+
+	//Read Frag File
+	string fragShaderCode;
+	ifstream fragShaderStream;
+
+	fragShaderStream.open(fragShaderFile, ifstream::in);
+
+	if (fragShaderStream.is_open()) {
+		stringstream sstr;
+		sstr << fragShaderStream.rdbuf();
+		fragShaderCode = sstr.str();
+		fragShaderStream.close();
+	}
+	else {
+		cout << "File does not open" << endl;
+		getchar();
+		return false;
+	}
+
+	GLint Result = GL_FALSE;
+	int InfoLogLength;
+
+
+	// Compile Vertex Shader
+	printf("Compiling shader : %s\n", vertexShaderFile);
+	char const * vertexSourcePointer = vertexShaderCode.c_str();
+	glShaderSource(myVertexObj, 1, &vertexSourcePointer, NULL);
+	glCompileShader(myVertexObj);
+
+	// Check Vertex Shader
+	glGetShaderiv(myVertexObj, GL_COMPILE_STATUS, &Result);
+	glGetShaderiv(myVertexObj, GL_INFO_LOG_LENGTH, &InfoLogLength);
+	if (InfoLogLength > 0) {
+		vector<char> VertexShaderErrorMessage(InfoLogLength + 1);
+		glGetShaderInfoLog(myVertexObj, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
+		printf("%s\n", &VertexShaderErrorMessage[0]);
+	}
+
+
+
+	// Compile Fragment Shader
+	printf("Compiling shader : %s\n", fragShaderFile);
+	char const * fragSourcePointer = fragShaderCode.c_str();
+	glShaderSource(myFragObj, 1, &fragSourcePointer, NULL);
+	glCompileShader(myFragObj);
+
+	// Check Fragment Shader
+	glGetShaderiv(myFragObj, GL_COMPILE_STATUS, &Result);
+	glGetShaderiv(myFragObj, GL_INFO_LOG_LENGTH, &InfoLogLength);
+	if (InfoLogLength > 0) {
+		vector<char> FragmentShaderErrorMessage(InfoLogLength + 1);
+		glGetShaderInfoLog(myFragObj, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
+		printf("%s\n", &FragmentShaderErrorMessage[0]);
+	}
+
+	// Link program
+	printf("Linking program\n");
+	myProgramObj = glCreateProgram();
+	glAttachShader(myProgramObj, myVertexObj);
+	glAttachShader(myProgramObj, myFragObj);
+	glLinkProgram(myProgramObj);
+
+	// Check Program
+	glGetProgramiv(myProgramObj, GL_LINK_STATUS, &Result);
+	glGetProgramiv(myProgramObj, GL_INFO_LOG_LENGTH, &InfoLogLength);
+
+	if (InfoLogLength > 0) {
+		vector<char> ProgramErrorMessage(InfoLogLength + 1);
+		glGetProgramInfoLog(myProgramObj, InfoLogLength, NULL, &ProgramErrorMessage[0]);
+		printf("%s\n", &ProgramErrorMessage[0]);
+		glDeleteShader(myVertexObj);
+		glDeleteShader(myFragObj);
+		glDeleteProgram(myProgramObj);
+		return false;
+	}
+
+	glUseProgram(myProgramObj);
+
+	glDeleteShader(myVertexObj);
+	glDeleteShader(myFragObj);
+
+	return true;
+}
+
+void display(void) { 
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); 
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 	glFlush(); 
 } 
 
-void main(int argc, char **argv) {
+int main(int argc, char **argv) {
+
 	glutInit(&argc, argv); 
-	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);  
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);  
 	glutInitWindowPosition(100, 100);  
 	glutInitWindowSize(320, 320); 
 	glutCreateWindow("Hello OpenGL"); 
-	glutDisplayFunc(renderScene);  
+	glutDisplayFunc(display);  
 	glClearColor(0.3f, 0.3f, 0.3f, 1.0f);  
 	glewInit();  
+	LoadShaders(vertexShaderFile, fragShaderFile);
 	glutMainLoop();
 }
+
+
